a103f57adc866b62fcc532310e5aee3438c84555
JafetMs <alexcero_@hotmail.com>
2024-05-18 16:54:33 -0600

    Login

---
+++ client/desktop/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { LoginForm  } from './Components/LoginForm'
+function App() {
+  
+
+  return (
+    <>
+     <LoginForm/>
+     
+
+    </>
+  )
+}
+
+export default App
---
+++ client/desktop/src/Components/LoginForm.jsx
@@ -0,0 +1,30 @@
+import tech from '../assets/tech.png';
+
+export const LoginForm = () => {
+  return (
+    <div className="login-container">
+    <div className="login-box">
+
+    <div className="login-welcome__img">
+          <img src={tech} alt="Technology" />
+    </div>
+
+    <h2>Welcome!</h2>
+        
+      
+     
+      <form>
+        <div className="input-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+  )
+}
