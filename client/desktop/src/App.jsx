72a8175352b515a4b769475a693d9527e0297604
Roger Pacheco <rogerp_00@outlook.com>
2024-05-19 12:55:09 -0600

    Repara inicio de proyecto client

--- client/desktop/src/App.jsx
+++ client/desktop/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
-import { LoginForm  } from './Components/LoginForm'
+import { LoginForm } from './components/LoginForm'
+
 function App() {
   
 
   return (
     <>
-     <LoginForm/>
-     
+
+     <LoginForm />
 
     </>
   )
